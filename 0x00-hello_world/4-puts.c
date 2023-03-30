bbf2ed4194a37de13773f100b010e9ccb1090759
adham-hussien33 <adhamhussien332005@gmail.com>
2023-03-30 04:24:39 -0700

    na

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
  * main - Entry point
  *
  * Description: Prints a message using the `puts` function
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
