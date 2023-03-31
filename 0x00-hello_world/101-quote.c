35291eec3a2dc7715f53de8daf92f2367c8e0c0a
adham-hussien33 <adhamhussien332005@gmail.com>
2023-03-31 11:49:34 -0700

    meeee

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: Prints a message to the standard error using the `write` function
- * Return: Always 1 (Error)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(msg) - 1;
-	ssize_t ret = write(2, msg, len);
-	return (ret == len) ? 1 : -1;
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1, quo, 59);
+return (1);
 }
 
