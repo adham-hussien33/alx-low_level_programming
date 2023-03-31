8a8dfeda5c06714fb6275ba6cc5054e45e651514
adham-hussien33 <adhamhussien332005@gmail.com>
2023-03-31 13:07:13 -0700

    me

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,13 @@
  */
 int main(void)
 {
-putchar(48);
-putchar(49);
-putchar(50);
-putchar(51);
-putchar(52);
-putchar(53);
-putchar(54);
-putchar(55);
-putchar(56);
-putchar(57);
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+
 putchar('\n');
 
 return (0);
