e5bcdacd3911e2d7ead3e111c69cc6c1a65919eb
adham-hussien33 <adhamhussien332005@gmail.com>
2023-04-03 19:06:02 -0700

    jskadbjvduhysbvyusDBA

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,35 @@
 #include "main.h"
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- */
-void jack_bauer(void)
-{
-int hours, minutes;
+#include <stdio.h>
 
-for (hours = 0; hours < 24; hours++)
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
 {
-for (minutes = 0; minutes < 60; minutes++)
+int num, mult, prod;
+
+for (num = 0; num <= 9; num++)
 {
-_putchar((hours / 10) + '0');
-_putchar((hours % 10) + '0');
-_putchar(':');
-_putchar((minutes / 10) + '0');
-_putchar((minutes % 10) + '0');
-_putchar('\n');
+_putchar(48);
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+
+prod = num * mult;
+
+if (prod <= 9)
+{
+_putchar(' ');
 }
+else
+{
+_putchar((prod / 10) + 48);
+}
+
+_putchar((prod % 10) + 48);
+}
+_putchar('\n');
 }
 }
 
