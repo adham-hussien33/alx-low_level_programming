c06022eb7ada87ffc2908f43ca2cb51e73f35d5c
adham-hussien33 <adhamhussien332005@gmail.com>
2023-04-03 18:20:33 -0700

    efhhjds

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: pointer to the string to modify
+ *
+ * Return: pointer to the modified string str
+ */
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+
+return (str);
+}
+
